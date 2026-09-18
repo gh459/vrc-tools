79a1af14d0b661e08ad99641580d6241418e770b
ll_kull <158707542+gh459@users.noreply.github.com>
2026-09-19 01:40:07 +0900

    Delete vrc-registry-tool/VRC設定インポート.ps1

--- vrc-registry-tool/VRC設定インポート.ps1
+++
@@ -1,5 +0,0 @@
-﻿# 移行先：既存設定のバックアップ
-reg export "HKCU\Software\VRChat\vrchat" "$env:USERPROFILE\Downloads\vrchat-settings-backup.reg" /y
-
-# 移行先：移行元の設定を取り込み
-reg import "$env:USERPROFILE\Downloads\vrchat-settings.reg"
